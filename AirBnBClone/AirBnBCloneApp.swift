692256c9b293c0b0b57500efaa5ce3e64df33ac5
Naleo-4 <anhbndc2004@gmail.com>
2024-07-29 19:59:16 +0700

    Initial Commit

---
+++ AirBnBClone/AirBnBCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBCloneApp.swift
+//  AirBnBClone
+//
+//  Created by Naleo on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
